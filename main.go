735a9dc39e9cab7fef4ade6efeda6cf746f080bf
申法宽 <sfk@live.cn>
2023-09-08 17:41:12 +0800

    url

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/url"
 	"os"
 )
 
@@ -21,7 +22,7 @@ func main() {
 		files = append(files, &audio{
 			Artist: d.Name()[0:8],
 			Name:   d.Name()[9:len(d.Name())],
-			Url:    "https://cdn.jsdelivr.net/gh/isfk/xiaohui/" + d.Name(),
+			Url:    "https://cdn.jsdelivr.net/gh/isfk/xiaohui/" + url.QueryEscape(d.Name()),
 		})
 	}
 
