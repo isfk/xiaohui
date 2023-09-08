441be7b71af1bc6f8846550cc963da990d0ffb67
申法宽 <sfk@live.cn>
2023-09-08 17:15:23 +0800

    player

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type audio struct {
+	Artist string `json:"artist"`
+	Name   string `json:"name"`
+	Url    string `json:"url"`
+}
+
+func main() {
+	dir, _ := os.ReadDir("./mp3")
+	files := []*audio{}
+
+	for _, d := range dir {
+		fmt.Println(d.Name()[9:(len(d.Name()) - 4)])
+		files = append(files, &audio{
+			Artist: d.Name()[0:8],
+			Name:   d.Name()[9:len(d.Name())],
+			Url:    "https://cdn.jsdelivr.net/gh/isfk/xiaohui/" + d.Name(),
+		})
+	}
+
+	vd, _ := json.Marshal(files)
+	fmt.Println(string(vd))
+	_ = os.WriteFile("list.js", vd, 666)
+	fmt.Println("ok")
+}
